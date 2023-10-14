ffc16cf681ed543d6addded2b11fabe07992f5ff
Dave Powers <djpowers89@gmail.com>
2023-10-14 16:26:02 -0400

    Test forecast validations error presence

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,3 +1,3 @@
 class Forecast < ApplicationRecord
-  validates_presence_of :temperature, :short_forecast, :icon, :number, :temperature_unit, :start_time, :end_time
+  validates_presence_of :temperature, :short, :icon, :number, :temperature_unit, :start_time, :end_time
 end
