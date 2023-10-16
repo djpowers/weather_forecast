882d1db80bf137a07d1f02e5e1b3bdc731d8ce73
Dave Powers <djpowers89@gmail.com>
2023-10-15 22:47:23 -0400

    Add comments to controller logic

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -5,10 +5,13 @@ class ForecastsController < ApplicationController
 
   def create
     @location = Location.find_or_create_by(inputted_address: params[:query])
+
+    # check for existing forecasts within determined zip code
     forecasts_by_zip_in_last_half_hour = Forecast.joins(:location).where(
       location: { postal_code: @location.postal_code }, created_at: 30.minutes.ago..Time.now
     ).order(created_at: :desc)
 
+    # return most recent forecast, if available
     if forecasts_by_zip_in_last_half_hour.present?
       flash[:notice] = 'Forecast data retrieved from cache'
       redirect_to location_forecast_path(
@@ -17,6 +20,7 @@ class ForecastsController < ApplicationController
       ) and return
     end
 
+    # set up client to retrieve forecast data from NWS API
     national_weather_service = NationalWeatherService.new(@location.latitude.truncate(4),
                                                           @location.longitude.truncate(4))
 
